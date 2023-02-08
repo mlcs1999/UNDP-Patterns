0efa3ebdcf0f248342fc640144aacf911af80107
mlcs1999 <milica.simic1999@gmail.com>
2023-02-08 21:10:03 +0100

    Singleton DB kraci

--- singleton/SingletonDBKraci.php
+++ singleton/SingletonDBKraci.php
@@ -1,5 +1,32 @@
 <?php
 
+class SinlgetonDBKraci
+{
+    //1. privatna static promenljiva
+    private static $conn = null;
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbName = "taskovi123";
+
+    //2. private konstruktor
+    private function __construct()
+    {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbName);
+    }
+
+    //3. public static metoda getIstance()
+    public static function konektuj()
+    {
+        if (self::$conn == null) {
+            self::$conn = new SinlgetonDBKraci();
+        }
+
+        return self::$conn;
+    }
+
+
+}
 
 
 
